32d4e3b12b33f41cc1e4921281b703477b5e2f9e
MouofoKayouDanela <mouofodanela@gmail.com>
2025-03-26 09:57:33 -0400

    Remplacement du message de la version d'essai par la misa à disposition du premier chapitre.

--- Langagio/Langagio/Program.cs
+++ Langagio/Langagio/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Bienvenue aux commandes de Langagio.");
-Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
+﻿
+   Console.WriteLine("Bienvenue aux commandes de Langagio.");
+
+            Console.WriteLine("Voici les chapitres disponibles : 1 (pronom)");
+            int numeroChapitre = obtenirUnNombreValide();
+
+            Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
