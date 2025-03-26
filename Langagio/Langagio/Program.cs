3305f96391b363551b1a14d58b858740f9b6ec58
MouofoKayouDanela <mouofodanela@gmail.com>
2025-03-26 09:59:55 -0400

    Ajout d'une fonction de validation de nombre.

--- Langagio/Langagio/Program.cs
+++ Langagio/Langagio/Program.cs
@@ -5,3 +5,21 @@
             int numeroChapitre = obtenirUnNombreValide();
 
             Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
+
+            
+        private static int obtenirUnNombreValide()
+        {
+            int entree;
+
+            Console.WriteLine("Entrez un nombre.");
+
+            bool estUnNombre = int.TryParse(Console.ReadLine(), out entree);
+
+            while (estUnNombre == false)
+            {
+                Console.WriteLine("L'entrée n'est pas un nombre. Essayez à nouveau.");
+                estUnNombre = int.TryParse(Console.ReadLine(), out entree);
+            }
+
+            return entree;
+        }
