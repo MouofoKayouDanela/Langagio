93539852b70c81a2bba13cb922e6021b16f68106
MouofoKayouDanela <mouofodanela@gmail.com>
2025-03-26 10:12:24 -0400

    Ajout de  la fonctionnalité de rappel du chapitre 2.

--- Langagio/Langagio/Program.cs
+++ Langagio/Langagio/Program.cs
@@ -1,10 +1,9 @@
 ﻿
    Console.WriteLine("Bienvenue aux commandes de Langagio.");
 
-            Console.WriteLine("Voici les chapitres disponibles : 1 (pronom)");
-            int numeroChapitre = obtenirUnNombreValide();
 
-            Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
+            Console.WriteLine("Voici les chapitres disponibles : 1 (pronom) 2 (verbe)");
+            int numeroChapitre = obtenirUnNombreValide();
 
             Console.WriteLine("Voici le rappel:");
             switch (numeroChapitre)
@@ -12,10 +11,16 @@
                 case 1:
                     Console.WriteLine("Dans ce chapitre sur le pronom, il faut se souvenir que la deuxième personne du singulier possède une terminaison particulière.");
                     break;
+                case 2:
+                    Console.WriteLine("Dans ce chapitre sur les verbes, il faut se souvenir que certains verbes sont irréguliers.");
+                    break;
                 default:
                     Console.WriteLine("Choix invalide. Aucun rappel affiché.");
                     break;
             }
+
+            Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
+
             
         private static int obtenirUnNombreValide()
         {
