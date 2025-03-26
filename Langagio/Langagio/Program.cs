40bb390ea62e2d12ed717f4a7994c7cd0a3d8358
MouofoKayouDanela <mouofodanela@gmail.com>
2025-03-26 10:18:08 -0400

    Remplacement de la fonctionnalite de rappel du chapitre 2 par la fonctionnalité de rappel du chapitre3.

--- Langagio/Langagio/Program.cs
+++ Langagio/Langagio/Program.cs
@@ -1,10 +1,8 @@
 ﻿
    Console.WriteLine("Bienvenue aux commandes de Langagio.");
 
-            Console.WriteLine("Voici les chapitres disponibles : 1 (pronom)");
+            Console.WriteLine("Voici les chapitres disponibles : 1 (pronom) 3 (participe passé)");
             int numeroChapitre = obtenirUnNombreValide();
-
-            Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
 
             Console.WriteLine("Voici le rappel:");
             switch (numeroChapitre)
@@ -12,10 +10,15 @@
                 case 1:
                     Console.WriteLine("Dans ce chapitre sur le pronom, il faut se souvenir que la deuxième personne du singulier possède une terminaison particulière.");
                     break;
+                case 3:
+                    Console.WriteLine("Dans ce chapitre sur le participe passé, il faut se souvenir de l'accord des participes passés.");
+                    break;
                 default:
                     Console.WriteLine("Choix invalide. Aucun rappel affiché.");
                     break;
             }
+
+            Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
             
         private static int obtenirUnNombreValide()
         {
