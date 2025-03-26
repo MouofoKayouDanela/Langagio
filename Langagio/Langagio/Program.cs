9e5bae2980e378743ef72b151bcf0f517370d5d0
MouofoKayouDanela <mouofodanela@gmail.com>
2025-03-26 10:03:09 -0400

    Ajout de la fonctionnalité de rapplel pour le chapitre 1.

--- Langagio/Langagio/Program.cs
+++ Langagio/Langagio/Program.cs
@@ -6,6 +6,16 @@
 
             Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
 
+            Console.WriteLine("Voici le rappel:");
+            switch (numeroChapitre)
+            {
+                case 1:
+                    Console.WriteLine("Dans ce chapitre sur le pronom, il faut se souvenir que la deuxième personne du singulier possède une terminaison particulière.");
+                    break;
+                default:
+                    Console.WriteLine("Choix invalide. Aucun rappel affiché.");
+                    break;
+            }
             
         private static int obtenirUnNombreValide()
         {
