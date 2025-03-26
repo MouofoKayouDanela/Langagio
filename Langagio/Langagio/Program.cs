7778bc7dbc235e303029068970147ebc6a2254b0
MouofoKayouDanela <mouofodanela@gmail.com>
2025-03-26 09:49:32 -0400

    Ajout de la version d'essai du logiciel.

--- Langagio/Langagio/Program.cs
+++ Langagio/Langagio/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bienvenue aux commandes de Langagio.");
+Console.WriteLine("Merci de votre essai du logiciel. Achetez la version complète pour traiter plus d'une fois consécutive.");
